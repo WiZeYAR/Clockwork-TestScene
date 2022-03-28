6fd9ecaf62c9f623867af6cac262caaf3ad7c834
Yaroslav Petrov <yaroslav.v.petrov@yandex.ru>
2022-03-28 21:28:25 +0200

    Implement quickload

--- src/systems/load_save.rs
+++ src/systems/load_save.rs
@@ -1,7 +1,11 @@
 use clockwork::prelude::{InputState, PhysicsState, VirtualKeyCode};
 use legion::{any, serialize::Canon, system, systems::CommandBuffer, Registry};
+use serde::de::DeserializeSeed;
 
 use crate::systems::{COMPONENT_REGISTRY, ENTITY_SERIALIZER};
+
+static SAVE_WORLD: &'static str = "save-world.json";
+static SAVE_PHYSICS: &'static str = "save-physics.json";
 
 #[system]
 pub fn load_save(cmd: &mut CommandBuffer, #[resource] input: &InputState) {
@@ -17,11 +21,32 @@ pub fn load_save(cmd: &mut CommandBuffer, #[resource] input: &InputState) {
             ))
             .unwrap();
             let physics =
-                serde_json::to_string(&*resources.get::<PhysicsState>().unwrap()).unwrap();
-            std::fs::write("save-world.json", world).unwrap();
-            std::fs::write("save-physics.json", physics).unwrap();
+                serde_json::to_string_pretty(&*resources.get::<PhysicsState>().unwrap()).unwrap();
+            std::fs::write(SAVE_WORLD, world).unwrap();
+            std::fs::write(SAVE_PHYSICS, physics).unwrap();
         });
     } else if input.pressed_keys().get(&VirtualKeyCode::F9).is_some() {
         // QUICK LOAD
+        cmd.exec_mut(|world, resources| {
+            let world_serializer: &Registry<_> = &COMPONENT_REGISTRY;
+            let entity_serializer: &Canon = &ENTITY_SERIALIZER;
+            *world = std::fs::read_to_string(SAVE_WORLD)
+                .map(|x| serde_json::from_str::<serde_json::Value>(&x).unwrap())
+                .map(|x| {
+                    world_serializer
+                        .as_deserialize(entity_serializer)
+                        .deserialize(&x)
+                        .unwrap()
+                })
+                .unwrap();
+            std::fs::read_to_string(SAVE_PHYSICS)
+                .map(|file| serde_json::from_str::<PhysicsState>(&file).unwrap())
+                .map(|saved_state| {
+                    let resource: &mut PhysicsState =
+                        &mut resources.get_mut::<PhysicsState>().unwrap();
+                    *resource = saved_state;
+                })
+                .unwrap();
+        });
     }
 }
